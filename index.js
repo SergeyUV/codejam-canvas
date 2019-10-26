fd713072d672d272dd185186ae3431d73dbe1ed5
Sergey Vnukov <skivun@hotmail.com>
2019-10-26 20:09:17 +0300

    feat: add basic object for draw on canvas

--- index.js
+++ index.js
@@ -6,19 +6,15 @@ function navChooseImgEvent(event){
 
     switch (this.id){
         case 'set_4x4':
-            
+            const myCanvasDrawer = new CanvasDrawer(document.getElementById('main_canvas'),4,4,[1,2,3]);
+            myCanvasDrawer.doDraw();
             break;
         case 'set_32x32':
             break;
         case 'set_png':
             break;
     }
-    console.log(this.id);
 }
-
- for ( let i = 0; i < document.getElementById('nav-choice_img').getElementsByTagName('li').length; i++){
-     document.getElementById('nav-choice_img').getElementsByTagName('li').item(i).addEventListener('click', navChooseImgEvent);
- };
 
 function clearMenuBack(){
     
@@ -26,3 +22,26 @@ function clearMenuBack(){
         document.getElementById('nav-choice_img').getElementsByTagName('li').item(i).classList.remove('nav_gray_back');
     };
 }
+
+for ( let i = 0; i < document.getElementById('nav-choice_img').getElementsByTagName('li').length; i++){
+    document.getElementById('nav-choice_img').getElementsByTagName('li').item(i).addEventListener('click', navChooseImgEvent);
+};
+
+function CanvasDrawer(canvasElement, datasizeX, datasizeY, dataPath) {
+    
+    this.that = this;
+    this.canvasElement = canvasElement;
+    this.datasizeX = datasizeX;
+    this.datasizeY = datasizeY;
+    this.ctx = undefined;
+    
+    if(canvasElement.getContext){
+        this.ctx = canvasElement.getContext('2d');
+    }
+
+    this.doDraw = function() {
+        this.ctx.fillStyle = "rgb(200,0,0)";
+        this.ctx.fillRect (10, 10, 55, 50);
+    }
+    
+};
