abe9681656ece76737dedbbd371aef769b6f23d6
Sergey Vnukov <skivun@hotmail.com>
2019-10-27 00:30:27 +0300

    feat: add CanvasDrawer constructor function

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function navChooseImgEvent(event){
 
     switch (this.id){
         case 'set_4x4':
-            const myCanvasDrawer = new CanvasDrawer(document.getElementById('main_canvas'),4,4,[1,2,3]);
+            const myCanvasDrawer = new CanvasDrawer(document.getElementById('main_canvas'), 4, 4, 'data/4x4.json');
             myCanvasDrawer.doDraw();
             break;
         case 'set_32x32':
@@ -23,10 +23,6 @@ function clearMenuBack(){
     };
 }
 
-for ( let i = 0; i < document.getElementById('nav-choice_img').getElementsByTagName('li').length; i++){
-    document.getElementById('nav-choice_img').getElementsByTagName('li').item(i).addEventListener('click', navChooseImgEvent);
-};
-
 function CanvasDrawer(canvasElement, datasizeX, datasizeY, dataPath) {
     
     this.that = this;
@@ -34,14 +30,39 @@ function CanvasDrawer(canvasElement, datasizeX, datasizeY, dataPath) {
     this.datasizeX = datasizeX;
     this.datasizeY = datasizeY;
     this.ctx = undefined;
-    
+    this.dataPath = dataPath;
+   
     if(canvasElement.getContext){
         this.ctx = canvasElement.getContext('2d');
     }
 
     this.doDraw = function() {
-        this.ctx.fillStyle = "rgb(200,0,0)";
-        this.ctx.fillRect (10, 10, 55, 50);
+        const deltaX = 256 / datasizeX;
+        const deltaY = 256 / datasizeY;
+        
+        fetch(this.dataPath)
+        .then(response => response.json())
+        .then(cdata => {
+            
+            let makecolor = function(d) {return d;};
+            
+            if(this.dataPath == 'data/4x4.json'){
+                makecolor = function(d){ return '#' + d; };
+            }
+
+            let rectY = 0;
+            for(let j = 0; j < datasizeY; j++, rectY+=deltaY) {
+                let rectX = 0;    
+                for(let i = 0; i < datasizeX; i++, rectX+=deltaX){
+                    this.ctx.fillStyle = makecolor(cdata[j][i]);
+                    this.ctx.fillRect (rectX, rectY, deltaX, deltaY);
+                }
+            }
+        });
     }
     
+};
+
+for ( let i = 0; i < document.getElementById('nav-choice_img').getElementsByTagName('li').length; i++){
+    document.getElementById('nav-choice_img').getElementsByTagName('li').item(i).addEventListener('click', navChooseImgEvent);
 };
